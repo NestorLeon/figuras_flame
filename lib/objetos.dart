2a9fd320824e2821983e8cfb8d9e806930fe3feb
Ismael <103325158+Isma0811@users.noreply.github.com>
2023-03-17 21:35:43 -0600

    Add files via upload

--- lib/objetos.dart
+++ lib/objetos.dart
@@ -3,9 +3,21 @@ import 'package:flame/text.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flame/game.dart';
+import 'package:flame/components.dart';
+import 'package:flame/geometry.dart';
 import 'dart:math';
 
-class Oso with Game {
+class Oso extends PositionComponent{
+  final Paint paint;
+
+
+   Oso({
+    required super.position,
+    required this.paint,
+    required super.size,
+    super.children,
+  }) : super();
+
   @override
   Future<void> onLoad() async {
     //super.onLoad();
@@ -24,55 +36,55 @@ class Oso with Game {
     ..color = Color.fromARGB(255, 6, 3, 0);
 
     final Cabeza =
-        Rect.fromCenter(center: Offset(400, 200), width: 200, height: 200);
+        Rect.fromCenter(center: Offset(size.x *.400, size.y *.200), width: size.x *.200, height: size.y *.200);
     final Cuerpo =
-        Rect.fromCenter(center: Offset(400, 400), width: 200, height: 250);
+        Rect.fromCenter(center: Offset(size.x *.400, size.y *.400), width: size.x *.200, height: size.y *.250);
     final Pata1 =
-        Rect.fromCenter(center: Offset(300, 330), width:150, height: 120);
+        Rect.fromCenter(center: Offset(size.x *.300, size.y *.330), width:size.x *.150, height: size.y *.120);
     final Pata2 =
-        Rect.fromCenter(center: Offset(500, 330), width:150, height: 120);
+        Rect.fromCenter(center: Offset(size.x *.500, size.y *.330), width:size.x *.150, height: size.y *.120);
     final Pata3 =
-        Rect.fromCenter(center: Offset(300, 500), width:150, height: 120);
+        Rect.fromCenter(center: Offset(size.x *.300, size.y *.500), width:size.x *.150, height: size.y *.120);
     final Pata4 =
-        Rect.fromCenter(center: Offset(500, 500), width:150, height: 120);
+        Rect.fromCenter(center: Offset(size.x *.500, size.y *.500), width:size.x *.150, height: size.y *.120);
       final Oreja1 =
-          Rect.fromCenter(center: Offset(300, 150), width:80, height: 80);
+          Rect.fromCenter(center: Offset(size.x *.300, size.y *.150), width:size.x *.080, height: size.y *.080);
       final Oreja2 =
-          Rect.fromCenter(center: Offset(500, 150), width:80, height: 80);
+          Rect.fromCenter(center: Offset(size.x *.500, size.y *.150), width:size.x *.080, height: size.y *.080);
     final Boca =
-        Rect.fromCenter(center: Offset(400, 220), width:100, height: 80);
+        Rect.fromCenter(center: Offset(size.x *.400, size.y *.220), width:size.x *.100, height: size.y *.080);
     
     final Huella1 =
-        Rect.fromCenter(center: Offset(280, 330), width:80, height: 80);
+        Rect.fromCenter(center: Offset(size.x *.280, size.y *.330), width:size.x *.080, height: size.y *.080);
     final Huella2 =
-        Rect.fromCenter(center: Offset(520, 330), width:80, height: 80);
+        Rect.fromCenter(center: Offset(size.x *.520, size.y *.330), width:size.x *.080, height: size.y *.080);
     final Huella3 =
-        Rect.fromCenter(center: Offset(280, 500), width:80, height: 80);
+        Rect.fromCenter(center: Offset(size.x *.280, size.y *.500), width:size.x *.080, height: size.y *.080);
     final Huella4 =
-        Rect.fromCenter(center: Offset(520, 500), width:80, height: 80);
+        Rect.fromCenter(center: Offset(size.x *.520, size.y *.500), width:size.x *.080, height: size.y *.080);
     
     final Ojo1 =
-        Rect.fromCenter(center: Offset(375, 180), width:30, height: 30);
+        Rect.fromCenter(center: Offset(size.x *.375, size.y *.180), width:size.x *.030, height: size.y *.030);
     final Ojo2 =
-        Rect.fromCenter(center: Offset(425, 180), width:30, height: 30);
+        Rect.fromCenter(center: Offset(size.x *.425, size.y *.180), width:size.x *.030, height: size.y *.030);
 
     final Nariz =
-        Rect.fromCenter(center: Offset(400, 200), width:30, height: 15);
+        Rect.fromCenter(center: Offset(size.x *.400, size.y *.200), width:size.x *.030, height: size.y *.015);
 
 
 
     canvas.drawOval(Oreja1, color2);
     canvas.drawOval(Oreja2, color2);
-    canvas.drawOval(Cabeza, color1);
-    canvas.drawOval(Cuerpo, color1);
+    canvas.drawOval(Cabeza, paint);
+    canvas.drawOval(Cuerpo, paint);
     canvas.drawOval(Ojo1, color4);
     canvas.drawOval(Ojo2, color4);
     canvas.drawOval(Boca, color2);
     canvas.drawOval(Nariz, color4);
-    canvas.drawOval(Pata1, color1);
-    canvas.drawOval(Pata2, color1);
-    canvas.drawOval(Pata3, color1);
-    canvas.drawOval(Pata4, color1);
+    canvas.drawOval(Pata1, paint);
+    canvas.drawOval(Pata2, paint);
+    canvas.drawOval(Pata3, paint);
+    canvas.drawOval(Pata4, paint);
     canvas.drawOval(Huella1, color3);
     canvas.drawOval(Huella2, color3);
     canvas.drawOval(Huella3, color3);
@@ -82,7 +94,7 @@ class Oso with Game {
 
 
     final rect = RRect.fromRectAndRadius(
-      Rect.fromCenter(center: Offset(300, 530), width: 100, height: 70),
+      Rect.fromCenter(center: Offset(size.x *.300, size.y *.530), width: size.x *.100, height: size.y *.070),
       Radius.circular(20),
     );
 
@@ -93,7 +105,17 @@ class Oso with Game {
 }
 
 
-class Guitarra with Game {
+class Guitarra extends PositionComponent {
+  final Paint paint;
+
+
+   Guitarra({
+    required super.position,
+    required this.paint,
+    required super.size,
+    super.children,
+  }) : super();
+
   @override
   Future<void> onLoad() async {
     //super.onLoad();
@@ -114,81 +136,81 @@ class Guitarra with Game {
     ..color = Color.fromARGB(255, 155, 155, 155);
 
     var path = Path()
-      ..moveTo(380, 250)
-      ..lineTo(100, 250)
-      ..lineTo(100, 310)
-      ..lineTo(380, 310)
+      ..moveTo(size.x *.380, size.y *.250)
+      ..lineTo(size.x *.100, size.y *.250)
+      ..lineTo(size.x *.100, size.y *.310)
+      ..lineTo(size.x *.380, size.y *.310)
       ..close();
 
     var path2 = Path()
-      ..moveTo(650, 230)
-      ..lineTo(660, 230)
-      ..lineTo(660, 330)
-      ..lineTo(650, 330)
+      ..moveTo(size.x *.650, size.y *.230)
+      ..lineTo(size.x *.660, size.y *.230)
+      ..lineTo(size.x *.660, size.y *.330)
+      ..lineTo(size.x *.650, size.y *.330)
       ..close();
 
     var Cuerda1 = Path()
-      ..moveTo(100, 250)
-      ..lineTo(655, 250)
-      ..lineTo(655, 255)
-      ..lineTo(100, 255)
+      ..moveTo(size.x *.100, size.y *.250)
+      ..lineTo(size.x *.655, size.y *.250)
+      ..lineTo(size.x *.655, size.y *.255)
+      ..lineTo(size.x *.100, size.y *.255)
       ..close();
 
     var Cuerda2 = Path()
-      ..moveTo(100, 260)
-      ..lineTo(655, 260)
-      ..lineTo(655, 265)
-      ..lineTo(100, 265)
+      ..moveTo(size.x *.100, size.y *.260)
+      ..lineTo(size.x *.655, size.y *.260)
+      ..lineTo(size.x *.655, size.y *.265)
+      ..lineTo(size.x *.100, size.y *.265)
       ..close();
     
     var Cuerda3 = Path()
-      ..moveTo(100, 270)
-      ..lineTo(655, 270)
-      ..lineTo(655, 275)
-      ..lineTo(100, 275)
+      ..moveTo(size.x *.100, size.y *.270)
+      ..lineTo(size.x *.655, size.y *.270)
+      ..lineTo(size.x *.655, size.y *.275)
+      ..lineTo(size.x *.100, size.y *.275)
       ..close();
 
     var Cuerda4 = Path()
-      ..moveTo(100, 280)
-      ..lineTo(655, 280)
-      ..lineTo(655, 285)
-      ..lineTo(100, 285)
+      ..moveTo(size.x *.100, size.y *.280)
+      ..lineTo(size.x *.655, size.y *.280)
+      ..lineTo(size.x *.655, size.y *.285)
+      ..lineTo(size.x *.100, size.y *.285)
       ..close();
 
     var Cuerda5 = Path()
-      ..moveTo(100, 290)
-      ..lineTo(655, 290)
-      ..lineTo(655, 295)
-      ..lineTo(100, 295)
+      ..moveTo(size.x *.100, size.y *.290)
+      ..lineTo(size.x *.655, size.y *.290)
+      ..lineTo(size.x *.655, size.y *.295)
+      ..lineTo(size.x *.100, size.y *.295)
       ..close();
 
     var Cuerda6 = Path()
-      ..moveTo(100, 300)
-      ..lineTo(655, 300)
-      ..lineTo(655, 305)
-      ..lineTo(100, 305)
+      ..moveTo(size.x *.100, size.y *.300)
+      ..lineTo(size.x *.655, size.y *.300)
+      ..lineTo(size.x *.655, size.y *.305)
+      ..lineTo(size.x *.100, size.y *.305)
       ..close();
 
     var path4 = Path()
-      ..moveTo(10, 230)
-      ..lineTo(100, 250)
-      ..lineTo(100, 310)
-      ..lineTo(10, 330)
+      ..moveTo(size.x *.010, size.y *.230)
+      ..lineTo(size.x *.100, size.y *.250)
+      ..lineTo(size.x *.100, size.y *.310)
+      ..lineTo(size.x *.010, size.y *.330)
       ..close();
 
     final Cuerpo1 =
-        Rect.fromCenter(center: Offset(490, 280), width: 240, height: 240);
+        Rect.fromCenter(center: Offset(size.x *.490, size.y *.280), width: size.x *.240, height: size.y *.240);
     final Cuerpo2 =
-        Rect.fromCenter(center: Offset(650, 280), width: 280, height: 280);
+        Rect.fromCenter(center: Offset(size.x *.650, size.y *.280), width: size.x *.280, height: size.y *.280);
     final Boca1 =
-        Rect.fromCenter(center: Offset(520, 280), width: 100, height: 100);
+        Rect.fromCenter(center: Offset(size.x *.520, size.y *.280), width: size.x *.100, height: size.y *.100);
     final Boca2 =
-        Rect.fromCenter(center: Offset(520, 280), width: 120, height: 120);
+        Rect.fromCenter(center: Offset(size.x *.520, size.y *.280), width: size.x *.120, height: size.y *.120);
 
     canvas.drawPath(path, colorC);
-    canvas.drawPath(path4, colorA);
-    canvas.drawOval(Cuerpo1, colorA);
-    canvas.drawOval(Cuerpo2, colorA);
+    canvas.drawPath(path4, paint);
+    canvas.drawOval(Cuerpo1, paint);
+    canvas.drawOval(Cuerpo2, paint);
     canvas.drawOval(Boca2, colorC);
     canvas.drawOval(Boca1, colorB);
     canvas.drawPath(path2, colorD);
@@ -204,3 +226,4 @@ class Guitarra with Game {
   @override
   void update(double deltaTime) {}
 }
+
