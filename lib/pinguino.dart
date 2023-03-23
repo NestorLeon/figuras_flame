daeb2097e05af852dccf38232b66ac8e7822b32d
JOSESR2000 <126103282+JOSESR2000@users.noreply.github.com>
2023-03-22 20:58:10 -0600

    Se corrigió error en size.y

--- lib/pinguino.dart
+++ lib/pinguino.dart
@@ -5,69 +5,82 @@ import 'package:flutter/material.dart';
 
 import 'tipos_de_forma.dart';
 
-class Penguin extends PositionComponent {
+class Iguana extends PositionComponent {
   final Paint paint;
   final FormaTypes forma;
 
-  Penguin({
+  Iguana({
     required super.position,
     required super.size,
     required this.paint,
     this.forma = FormaTypes.rectanguloVertical,
     super.children,
   }) : super();
-
   @override
   void render(Canvas canvas) {
+    super.render(canvas);
+
     double r = size.x / 2;
-    // Cuerpo
+
+    // Cola
+    final tailPath = Path()
+      ..moveTo(r, size.y)
+      ..lineTo(r * 1.3, 3 * r)
+      ..lineTo(r * 1.3, size.y + r)
+      ..close();
+    canvas.drawPath(tailPath, paint);
+
     canvas.drawLine(
-      Offset(r, r),
-      Offset(r, size.y),
-      paint..strokeWidth = r * 2,
+      Offset(2 * r, size.y),
+      Offset(0 * r, 2 * r),
+      paint..strokeWidth = r / 4,
     );
     canvas.drawLine(
-      Offset(r, r),
+      Offset(2 * r, 2 * r),
+      Offset(0 * r, size.y),
+      paint..strokeWidth = r / 4,
+    );
+    // Cuerpo
+    canvas.drawLine(
+      Offset(r, 1.5 * r),
+      Offset(r, size.y),
+      paint..strokeWidth = r,
+    );
+    canvas.drawLine(
+      Offset(r, 2 * r),
       Offset(r, size.y - (r / 4)),
       Paint()
-        ..color = Colors.white
-        ..strokeWidth = r * 1.5,
+        ..color = const Color.fromARGB(97, 197, 226, 101)
+        ..strokeWidth = r / 2,
     );
     // Cabeza
     canvas.drawCircle(Offset(r, r), r, paint);
-    canvas.drawCircle(Offset(r, r), r,
-        Paint()..color = const Color.fromARGB(20, 255, 255, 255));
-    canvas.drawCircle(Offset(r, r), r * 0.9,
-        Paint()..color = const Color.fromARGB(10, 255, 255, 255));
     // Ojos
     final leftEyePosition = Offset(r * 1 / 2, r);
     final rightEyePosition = Offset(r * 3 / 2, r);
     canvas.drawCircle(
       leftEyePosition,
-      (1 / 16) * size.x,
+      (1 / 8) * size.x,
       Paint()..color = Colors.white,
     );
     canvas.drawCircle(
       leftEyePosition,
-      (1 / 20) * size.x,
+      (1 / 16) * size.x,
       Paint()..color = Colors.black,
     );
     canvas.drawCircle(
       rightEyePosition,
-      (1 / 16) * size.x,
+      (1 / 8) * size.x,
       Paint()..color = Colors.white,
     );
     canvas.drawCircle(
       rightEyePosition,
-      (1 / 20) * size.x,
+      (1 / 16) * size.x,
       Paint()..color = Colors.black,
     );
-    // Pico
-    final mouthPath = Path();
-    mouthPath.moveTo(r, r * 1.3);
-    mouthPath.lineTo(r * 1, r * 1.7);
-    mouthPath.lineTo(r * 1.3, r * 1.5);
-    final mouthPaint = Paint()..color = Colors.orange;
-    canvas.drawPath(mouthPath, mouthPaint);
+    // Nariz
+    final nosePosition = Offset(r, r * 1.2);
+    canvas.drawCircle(nosePosition, r / 10,
+        Paint()..color = const Color.fromARGB(69, 0, 0, 0));
   }
 }
