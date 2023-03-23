e5370c9a1556d3502a8b9277db0a674ad14224a9
JOSESR2000 <126103282+JOSESR2000@users.noreply.github.com>
2023-03-22 20:32:18 -0600

    Se corrigió error de en size.y

--- lib/iguana.dart
+++ lib/iguana.dart
@@ -21,6 +21,38 @@ class Iguana extends PositionComponent {
     super.render(canvas);
 
     double r = size.x / 2;
+
+    // Cola
+    final tailPath = Path()
+      ..moveTo(r, size.y)
+      ..lineTo(r * 1.3, 3 * r)
+      ..lineTo(r * 1.3, size.y + r)
+      ..close();
+    canvas.drawPath(tailPath, paint);
+
+    canvas.drawLine(
+      Offset(2 * r, size.y),
+      Offset(0 * r, 2 * r),
+      paint..strokeWidth = r / 4,
+    );
+    canvas.drawLine(
+      Offset(2 * r, 2 * r),
+      Offset(0 * r, size.y),
+      paint..strokeWidth = r / 4,
+    );
+    // Cuerpo
+    canvas.drawLine(
+      Offset(r, 1.5 * r),
+      Offset(r, size.y),
+      paint..strokeWidth = r,
+    );
+    canvas.drawLine(
+      Offset(r, 2 * r),
+      Offset(r, size.y - (r / 4)),
+      Paint()
+        ..color = const Color.fromARGB(97, 197, 226, 101)
+        ..strokeWidth = r / 2,
+    );
     // Cabeza
     canvas.drawCircle(Offset(r, r), r, paint);
     // Ojos
@@ -28,50 +60,27 @@ class Iguana extends PositionComponent {
     final rightEyePosition = Offset(r * 3 / 2, r);
     canvas.drawCircle(
       leftEyePosition,
-      (1 / 16) * size.y,
+      (1 / 8) * size.x,
       Paint()..color = Colors.white,
     );
     canvas.drawCircle(
-        leftEyePosition, (1 / 32) * size.y, Paint()..color = Colors.black);
+      leftEyePosition,
+      (1 / 16) * size.x,
+      Paint()..color = Colors.black,
+    );
     canvas.drawCircle(
-        rightEyePosition, (1 / 16) * size.y, Paint()..color = Colors.white);
+      rightEyePosition,
+      (1 / 8) * size.x,
+      Paint()..color = Colors.white,
+    );
     canvas.drawCircle(
-        rightEyePosition, (1 / 32) * size.y, Paint()..color = Colors.black);
+      rightEyePosition,
+      (1 / 16) * size.x,
+      Paint()..color = Colors.black,
+    );
     // Nariz
     final nosePosition = Offset(r, r * 1.2);
     canvas.drawCircle(nosePosition, r / 10,
         Paint()..color = const Color.fromARGB(69, 0, 0, 0));
-
-    // Cola
-    final tailPath = Path()
-      ..moveTo(r, 4 * r)
-      ..lineTo(r * 1.3, 3 * r)
-      ..lineTo(r * 1.3, r * 5)
-      ..close();
-    canvas.drawPath(tailPath, paint);
-
-    canvas.drawLine(
-      Offset(2 * r, 4 * r),
-      Offset(0 * r, 2 * r),
-      paint..strokeWidth = r / 4,
-    );
-    canvas.drawLine(
-      Offset(2 * r, 2 * r),
-      Offset(0 * r, 4 * r),
-      paint..strokeWidth = r / 4,
-    );
-    // Cuerpo
-    canvas.drawLine(
-      Offset(r, 1.5 * r),
-      Offset(r, 4 * r),
-      paint..strokeWidth = r,
-    );
-    canvas.drawLine(
-      Offset(r, 2 * r),
-      Offset(r, 3.8 * r),
-      Paint()
-        ..color = const Color.fromARGB(97, 197, 226, 101)
-        ..strokeWidth = r / 2,
-    );
   }
 }
