a7318aedfdb36c9937f2bf563413cd3f8a680247
EdgarTorres1 <125910284+EdgarT0rres1@users.noreply.github.com>
2023-03-27 10:12:02 -0600

    Add files via upload

--- lib/mochila.dart
+++ lib/mochila.dart
@@ -1,14 +1,10 @@
 library figuras_flame;
-import 'dart:js_util';
 import 'dart:ui';
-import 'package:figuras_flame/tipos_de_forma.dart';
-import 'package:flame/extensions.dart';
-import 'package:flame/game.dart';
 import 'package:flame/components.dart';
 import 'package:flutter/material.dart';
+import 'tipos_de_forma.dart';
 
-
-
+ 
 class Mochila extends PositionComponent {
   final Paint paint;
    final FormaTypes forma;
@@ -40,116 +36,116 @@ class Mochila extends PositionComponent {
     //Dibujar el tronco
     //linea p1 y p2
     canvas.drawLine(
-      Offset(position.x + size.x*1/3,position.y),
-      Offset(position.x + size.x*2/3,position.y),
+      Offset(size.x*1/3,size.y-size.y),
+      Offset(size.x*2/3,size.y-size.y),
       paint..strokeWidth = 4,
       
     );
     //linea p1 y p3
        canvas.drawLine(
-      Offset(position.x + size.x*1/3,position.y),
-      Offset(position.x + size.x*1/3,position.y+1/5*size.y),
+      Offset( size.x*1/3,size.y-size.y),
+      Offset( size.x*1/3,1/5*size.y),
       paint..strokeWidth = 4,
       
     );
     //linea p2 y p4
         canvas.drawLine(
-      Offset(position.x + size.x*2/3,position.y),
-      Offset(position.x + size.x*2/3,position.y+1/5*size.y),
+      Offset( size.x*2/3,size.y-size.y),
+      Offset( size.x*2/3,1/5*size.y),
       paint..strokeWidth = 4,
       
     );
         canvas.drawLine(
-      Offset(position.x + size.x*1/6,position.y+1/5*size.y),
-      Offset(position.x + size.x*5/6,position.y+1/5*size.y),
+      Offset( size.x*1/6,1/5*size.y),
+      Offset(size.x*5/6,1/5*size.y),
       paint..strokeWidth = 4,
       
     );
         canvas.drawLine(
-      Offset(position.x + size.x*1/6,position.y+1/5*size.y),
-      Offset(position.x + size.x*1/6,position.y+1*size.y),
+      Offset( size.x*1/6,1/5*size.y),
+      Offset(size.x*1/6,1*size.y),
       paint..strokeWidth = 4,
       
     );
 
     canvas.drawLine(
-      Offset(position.x + size.x*5/6,position.y+1/5*size.y),
-      Offset(position.x + size.x*5/6,position.y+1*size.y),
+      Offset(size.x*5/6,1/5*size.y),
+      Offset( size.x*5/6,1*size.y),
       paint..strokeWidth = 4,
     );
     canvas.drawLine(
-      Offset(position.x + size.x*1/6,position.y+1*size.y),
-      Offset(position.x + size.x*5/6,position.y + 1*size.y),
+      Offset( size.x*1/6,1*size.y),
+      Offset( size.x*5/6, 1*size.y),
       paint..strokeWidth = 4,
     );
      canvas.drawLine(
-      Offset(position.x + size.x*5/12,position.y+1/20*size.y),
-      Offset(position.x + size.x*7/12,position.y + 1/20*size.y),
+      Offset(size.x*5/12,1/20*size.y),
+      Offset(size.x*7/12, 1/20*size.y),
       paint..strokeWidth = 4,
     );
      canvas.drawLine(
-      Offset(position.x + size.x*5/12,position.y+1/20*size.y),
-      Offset(position.x + size.x*5/12,position.y + 1/5*size.y),
+      Offset( size.x*5/12,1/20*size.y),
+      Offset( size.x*5/12, 1/5*size.y),
       paint..strokeWidth = 4,
     );
        canvas.drawLine(
-       Offset(position.x + size.x*7/12,position.y + 1/20*size.y),
-       Offset(position.x + size.x*7/12,position.y+1/5*size.y),
+       Offset(  size.x*7/12,1/20*size.y),
+       Offset(size.x*7/12,1/5*size.y),
       paint..strokeWidth = 4,
     );
        canvas.drawLine(
-       Offset(position.x + size.x*1/3,position.y + 7/10*size.y),
-       Offset(position.x + size.x*2/3,position.y+7/10*size.y),
+       Offset( size.x*1/3, 7/10*size.y),
+       Offset( size.x*2/3,7/10*size.y),
       paint..strokeWidth = 4,
     );
           canvas.drawLine(
-       Offset(position.x + size.x*1/3,position.y + 7/10*size.y),
-       Offset(position.x + size.x*1/3,position.y+9/10*size.y),
+       Offset( size.x*1/3, 7/10*size.y),
+       Offset(  size.x*1/3,9/10*size.y),
       paint..strokeWidth = 4,
     );
         canvas.drawLine(
-       Offset(position.x + size.x*2/3,position.y + 7/10*size.y),
-       Offset(position.x + size.x*2/3,position.y+9/10*size.y),
+       Offset( size.x*2/3, 7/10*size.y),
+       Offset( size.x*2/3,9/10*size.y),
       paint..strokeWidth = 4,
     );
      canvas.drawLine(
-       Offset(position.x + size.x*1/3,position.y + 9/10*size.y),
-       Offset(position.x + size.x*2/3,position.y+9/10*size.y),
+       Offset( size.x*1/3, 9/10*size.y),
+       Offset( size.x*2/3,9/10*size.y),
       paint..strokeWidth = 4,
     );
        canvas.drawLine(
-       Offset(position.x + size.x*1/3,position.y + 4/10*size.y),
-       Offset(position.x + size.x*2/3,position.y+4/10*size.y),
+       Offset( size.x*1/3, 4/10*size.y),
+       Offset( size.x*2/3,4/10*size.y),
       paint..strokeWidth = 4,
     );
          canvas.drawLine(
-       Offset(position.x + size.x*1/3,position.y + 4/10*size.y),
-       Offset(position.x + size.x*1/3,position.y+5/10*size.y),
+       Offset( size.x*1/3, 4/10*size.y),
+       Offset( size.x*1/3,5/10*size.y),
       paint..strokeWidth = 4,
     );
        canvas.drawLine(
-       Offset(position.x + size.x*1/3,position.y + 5/10*size.y),
-       Offset(position.x + size.x*2/3,position.y+5/10*size.y),
+       Offset(size.x*1/3, 5/10*size.y),
+       Offset( size.x*2/3,5/10*size.y),
       paint..strokeWidth = 4,);
        canvas.drawLine(
-       Offset(position.x + size.x*2/3,position.y + 4/10*size.y),
-       Offset(position.x + size.x*2/3,position.y+5/10*size.y),
+       Offset( size.x*2/3, 4/10*size.y),
+       Offset( size.x*2/3,5/10*size.y),
       paint..strokeWidth = 4,);
         canvas.drawLine(
-       Offset(position.x + size.x*5/6,position.y + 6/10*size.y),
-       Offset(position.x + size.x*1,position.y+19/20*size.y),
+       Offset( size.x*5/6,  6/10*size.y),
+       Offset( size.x*1,19/20*size.y),
       paint..strokeWidth = 4,);
       canvas.drawLine(
-       Offset(position.x + size.x*1,position.y + 19/20*size.y),
-       Offset(position.x + size.x*5/6,position.y+19/20*size.y),
+       Offset(  size.x+1, 19/20*size.y),
+       Offset( size.x*5/6,19/20*size.y),
       paint..strokeWidth = 4,);
      canvas.drawLine(
-       Offset(position.x + size.x*1/6,position.y + 6/10*size.y),
-       Offset(position.x ,position.y+19/20*size.y),
+       Offset( size.x*1/6, 6/10*size.y),
+       Offset(size.x-size.x ,19/20*size.y),
       paint..strokeWidth = 4,);
        canvas.drawLine(
-       Offset(position.x,position.y + 19/20*size.y),
-       Offset(position.x + size.x*1/6,position.y+19/20*size.y),
+       Offset(size.x-size.x,19/20*size.y),
+       Offset(0+size.x*1/6,19/20*size.y),
       paint..strokeWidth = 4,);
 
 
