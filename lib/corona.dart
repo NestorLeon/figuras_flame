4124da41b157a6865d5c8fa9c9add0414f8bc5d0
Benjamín <benja13lara@gmail.com>
2023-03-27 21:45:19 -0600

    ultima modificación de figuras

--- lib/corona.dart
+++ lib/corona.dart
@@ -5,6 +5,10 @@ import 'package:flutter/material.dart';
 
 class Corona extends PositionComponent {
   final Paint paint;
+  var X = 1.57;
+  var Y = 2.03;
+  var A = .03;
+  var B = .045;
 
   Corona({
     required super.position,
@@ -20,55 +24,55 @@ class Corona extends PositionComponent {
     super.render(canvas);
 
     var path = Path()
-      ..moveTo(size.x * 0.085, size.y * 0.156)
-      ..lineTo(size.x * 0.055, size.y * 0.168)
-      ..lineTo(size.x * 0.136, size.y * 0.460)
-      ..lineTo(size.x * 0.558, size.y * 0.460)
-      ..lineTo(size.x * 0.641, size.y * 0.173)
-      ..lineTo(size.x * 0.606, size.y * 0.156)
-      ..lineTo(size.x * 0.449, size.y * 0.335)
-      ..lineTo(size.x * 0.365, size.y * 0.098)
-      ..lineTo(size.x * 0.330, size.y * 0.098)
-      ..lineTo(size.x * 0.242, size.y * 0.332)
+      ..moveTo(X * size.x * (0.085 - A), Y * size.y * (0.156 - B))
+      ..lineTo(X * size.x * (0.055 - A), Y * size.y * (0.168 - B))
+      ..lineTo(X * size.x * (0.136 - A), Y * size.y * (0.460 - B))
+      ..lineTo(X * size.x * (0.558 - A), Y * size.y * (0.460 - B))
+      ..lineTo(X * size.x * (0.641 - A), Y * size.y * (0.173 - B))
+      ..lineTo(X * size.x * (0.606 - A), Y * size.y * (0.156 - B))
+      ..lineTo(X * size.x * (0.449 - A), Y * size.y * (0.335 - B))
+      ..lineTo(X * size.x * (0.365 - A), Y * size.y * (0.098 - B))
+      ..lineTo(X * size.x * (0.330 - A), Y * size.y * (0.098 - B))
+      ..lineTo(X * size.x * (0.242 - A), Y * size.y * (0.332 - B))
       //
-      ..moveTo(size.x * 0.128, size.y * 0.461)
-      ..lineTo(size.x * 0.125, size.y * 0.535)
-      ..lineTo(size.x * 0.567, size.y * 0.539)
-      ..lineTo(size.x * 0.565, size.y * 0.459)
-      ..lineTo(size.x * 0.128, size.y * 0.460)
+      ..moveTo(X * size.x * (0.128 - A), Y * size.y * (0.461 - B))
+      ..lineTo(X * size.x * (0.125 - A), Y * size.y * (0.535 - B))
+      ..lineTo(X * size.x * (0.567 - A), Y * size.y * (0.539 - B))
+      ..lineTo(X * size.x * (0.565 - A), Y * size.y * (0.459 - B))
+      ..lineTo(X * size.x * (0.128 - A), Y * size.y * (0.460 - B))
       //
       ..close();
 
-    
-    canvas.drawCircle(Offset(size.x * 0.070, size.y * 0.156), size.x * 0.040, paint);
-    canvas.drawCircle(Offset(size.x * 0.348, size.y * 0.098), size.x * 0.040, paint);
-    canvas.drawCircle(Offset(size.x * 0.626, size.y * 0.170), size.x * 0.040, paint);    
+    canvas.drawCircle(
+        Offset(X * size.x * (0.070 - A), Y * size.y * (0.156 - B)), X * size.x * (0.040), paint);
+    canvas.drawCircle(
+        Offset(X * size.x * (0.348 - A), Y * size.y * (0.098 - B)), X * size.x * (0.040), paint);
+    canvas.drawCircle(
+        Offset(X * size.x * (0.626 - A), Y * size.y * (0.170 - B)), X * size.x * (0.040), paint);
     canvas.drawPath(path, paint);
-    
+
     canvas.drawLine(
-      Offset(size.x * 0.345, size.y * 0.306),
-      Offset(size.x * 0.375, size.y * 0.365),
+      Offset(X * size.x * (0.345 - A), Y * size.y * (0.306 - B)),
+      Offset(X * size.x * (0.375 - A), Y * size.y * (0.365 - B)),
       paint,
     );
 
     canvas.drawLine(
-      Offset(size.x * 0.375, size.y * 0.365),
-      Offset(size.x * 0.344, size.y * 0.428),
+      Offset(X * size.x * (0.375 - A), Y * size.y * (0.365 - B)),
+      Offset(X * size.x * (0.344 - A), Y * size.y * (0.428 - B)),
       paint,
     );
 
     canvas.drawLine(
-      Offset(size.x * 0.344, size.y * 0.428),
-      Offset(size.x * 0.313, size.y * 0.364),
+      Offset(X * size.x * (0.344 - A), Y * size.y * (0.428 - B)),
+      Offset(X * size.x * (0.313 - A), Y * size.y * (0.364 - B)),
       paint,
     );
 
     canvas.drawLine(
-      Offset(size.x * 0.313, size.y * 0.364),
-      Offset(size.x * 0.345, size.y * 0.306),
+      Offset(X * size.x * (0.313 - A), Y * size.y * (0.364 - B)),
+      Offset(X * size.x * (0.345 - A), Y * size.y * (0.306 - B)),
       paint,
     );
-
   }
-} 
-
+}
