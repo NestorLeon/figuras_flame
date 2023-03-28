8835964a8f11b3862575b3d631a8661ae2b319be
DanielDiazZavala <126103561+DanielDiazZavala@users.noreply.github.com>
2023-03-27 22:09:09 -0600

    Add files via upload

--- lib/pepe_grillo.dart
+++ lib/pepe_grillo.dart
@@ -1,3 +1,4 @@
+library figuras_flame;
 import 'package:flame/components.dart';
 import 'package:flame/palette.dart';
 import 'package:flame/text.dart';
@@ -27,55 +28,55 @@ class Grillo extends PositionComponent {
     final colorAzul = Paint()
     ..color = Color.fromARGB(255, 53, 121, 167);
     final colorRojo = Paint()
-    ..color = Color.fromARGB(255, 255, 0, 0);
+    ..color = Color.fromARGB(255, 142, 5, 5);
     final colorNegro = Paint()
     ..color = Color.fromARGB(255, 0, 0, 0);
     final colorBlanco = Paint()
     ..color = Color.fromARGB(255, 255, 255, 255);
 
     final Cabeza =
-        Rect.fromCenter(center: Offset(size.x*.400, size.x*.400), width: size.x*.200, height: size.x*.230);
+        Rect.fromCenter(center: Offset(size.x*.500, size.y*.780), width: size.x*.5500, height: size.y*.43);
     final Ojo1 =
-        Rect.fromCenter(center: Offset(size.x*.350, size.x*.380), width:size.x*.050, height: size.x*.070);
+        Rect.fromCenter(center: Offset(size.x*.390, size.y*.705), width:size.x*.115, height: size.y*.120);
     final Ojo2 =
-        Rect.fromCenter(center: Offset(size.x*.450, size.x*.380), width:size.x*.050, height: size.x*.070);
+        Rect.fromCenter(center: Offset(size.x*.610, size.y*.705), width:size.x*.115, height: size.y*.120);
 
     final Pupila1 =
-        Rect.fromCenter(center: Offset(size.x*.350, size.x*.390), width:size.x*.040, height: size.x*.050);
+        Rect.fromCenter(center: Offset(size.x*.390, size.y*.725), width:size.x*.080, height: size.y*.090);
     final Pupila2 =
-        Rect.fromCenter(center: Offset(size.x*.450, size.x*.390), width:size.x*.040, height: size.x*.050);
+        Rect.fromCenter(center: Offset(size.x*.610, size.y*.725), width:size.x*.080, height: size.y*.090);
 
     final cachete1=
-    Rect.fromCenter(center: Offset(size.x*.360 ,size.x*.450), width: size.x*.120, height: size.x*.090);
+    Rect.fromCenter(center: Offset(size.x*.410 ,size.y*.845), width: size.x*.420, height: size.y*.200);
     
     final cachete2=
-    Rect.fromCenter(center: Offset(size.x*.440,size.x*.450), width: size.x*.120, height: size.x*.090);
+    Rect.fromCenter(center: Offset(size.x*.590,size.y*.845), width: size.x*.420, height: size.y*.200);
     
     var Bigote= Path()
-    ..moveTo(size.x*.400, size.x*.460)
-    ..lineTo(size.x*.430, size.x*.435)
-    ..lineTo(size.x*.370, size.x*.435)
+    ..moveTo(size.x*.500, size.y*.870)
+    ..lineTo(size.x*.600, size.y*.835)
+    ..lineTo(size.x*.400, size.y*.835)
     ..close();
     var Bigote_Fondo= Path()
-    ..moveTo(size.x*.400, size.x*.457)
-    ..lineTo(size.x*.433, size.x*.429)
-    ..lineTo(size.x*.367, size.x*.429)
+    ..moveTo(size.x*.495, size.y*.865)
+    ..lineTo(size.x*.595, size.y*.825)
+    ..lineTo(size.x*.405, size.y*.825)
     ..close();
     var Sombrero = Path()
-      ..moveTo(size.x*.250, size.x*.330)
-      ..lineTo(size.x*.550, size.x*.330)
-      ..lineTo(size.x*.550, size.x*.300)
-      ..lineTo(size.x*.480, size.x*.300)
-      ..lineTo(size.x*.510, size.x*.100)
-      ..lineTo(size.x*.290, size.x*.100)
-      ..lineTo(size.x*.320, size.x*.300)
-      ..lineTo(size.x*.250, size.x*.300)
+      ..moveTo(0, size.y*.650)
+      ..lineTo(size.x, size.y*.650)//anchor
+      ..lineTo(size.x, size.y*.565)
+      ..lineTo(size.x*.725, size.y*.565)
+      ..lineTo(size.x*.840, 0)
+      ..lineTo(size.x*.160, 0)
+      ..lineTo(size.x*.275, size.y*.565)
+      ..lineTo(0, size.y*.565)
       ..close();
     
     canvas.drawOval(Cabeza, colorVerde);
   
     canvas.drawPath(Sombrero, paint);
-    canvas.drawRect( Rect.fromCenter(center: Offset(size.x*.400,size.x*.287), width: size.x*.167, height: size.x*.025),colorRojo);
+    canvas.drawRect( Rect.fromCenter(center: Offset(size.x*.5,size.y*.530), width: size.x*.490, height: size.y*.075),colorRojo);
     canvas.drawOval(Ojo1, colorBlanco);
     canvas.drawOval(Ojo2, colorBlanco);
     canvas.drawOval(Pupila1, colorNegro);
