8508bb881218b41c9fc750b8c0cc8a26d6e9f168
EDUARDO-DLTA <126103374+EDUARDO-DLTA@users.noreply.github.com>
2023-03-29 11:57:43 -0600

    Add files via upload

---
+++ lib/ballena.dart
@@ -0,0 +1,67 @@
+library figuras_flame;
+
+import 'dart:ui';
+import 'package:flame/components.dart';
+import 'package:flutter/material.dart';
+
+class Ballena extends PositionComponent {
+  final Paint paint;
+    Ballena({
+    required super.position,
+    required this.paint,
+    required super.size,
+    super.children,
+  }) : super();
+
+  @override
+  void render(Canvas canvas) {
+    super.render(canvas);
+
+    // Cabeza
+    double radio =400;
+    const headRect = Rect.fromLTWH(100, 10, 400, 300);
+    canvas.drawOval(Rect.fromLTWH(0,0,size.x,size.y), paint);
+
+    //double radio2=400;
+    //const papada = Rect.fromLTWH(200, 160, 250, 150);
+    //final papadapaint = Paint()..color=Colors.white;
+    //canvas.drawOval(Rect.fromLTWH(100,10,width,height), paint);
+
+    // Ojos+
+    final posiscionojo=Offset(((3/4) * size.x), ((3/8) * size.y));
+
+    canvas.drawCircle(
+      posiscionojo, 1/10 * size.x, Paint()..color=Colors.white,
+    );
+
+    final posicionpupila=Offset(((3/4) * size.x), ((3/8) * size.y));
+    canvas.drawCircle(
+      posicionpupila, 1/20 * size.x, Paint()..color=Colors.black,
+    );
+
+    //final posicionpapada=Offset(((3/4) * size.x), ((3/8) * size.y));
+    
+      
+    
+
+   // const ojo2 = Offset(430, 100);
+    //const eyeRadio = 30.0;
+    //final eyecolor = Paint()..color = Colors.white;
+    //canvas.drawCircle(ojo2, eyeRadio, eyecolor);
+
+    //const ojo = Offset(430, 100);
+    //const eyeRadius = 15.0;
+    //final eyePaint = Paint()..color = const Color.fromARGB(255, 66, 66, 66);
+    //canvas.drawCircle(ojo, eyeRadius, eyePaint);
+    
+    
+
+
+    
+    
+
+    
+   
+    
+  }
+}
