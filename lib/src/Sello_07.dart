c655f14ec279b79cbe13de38c7bbe4b39db6fcfd
BrayanD07 <brayaneve@hotmail.com>
2023-08-31 17:03:21 -0600

    primera clase

---
+++ lib/src/Sello_07.dart
@@ -0,0 +1,32 @@
+library figuras_flame;
+
+import 'dart:ui';
+import 'package:figuras_flame/src/tipos_de_forma.dart';
+import 'package:flame/components.dart';
+
+class Mono extends PositionComponent {
+  final FormaTypes forma;
+  final Paint paint;
+
+  Mono({
+    required super.position,
+    required this.paint,
+    required super.size,
+    this.forma = FormaTypes.rectanguloVertical,
+    super.children,
+  }) : super();
+
+  @override
+  void render(Canvas canvas) {
+    super.render(canvas);
+    
+    double radio = size.x / 2;
+
+    //Dibujar circulo
+    canvas.drawCircle(
+      Offset(radio, radio),
+      radio,
+      paint..strokeWidth = 4,
+    );
+  }
+}
