226f41adb0c642f56f03278a8fa2230e0b1d67a8
BrayanD07 <brayaneve@hotmail.com>
2023-09-12 16:54:32 -0600

    c

--- lib/src/Sello_07.dart
+++ lib/src/Sello_07.dart
@@ -61,7 +61,8 @@ class Sello_07 extends PositionComponent {
       ..moveTo(((1 / 9.3) * size.x), ((1.45/ 8) * size.y))
       ..lineTo(((1 / 7) * size.x), ((1.45/ 8) * size.y))
 
-      ..moveTo(((1 / 7) * size.x), ((1.45/ 8) * size.y))
+      //
+      ..moveTo(((1 / 7) * size.x), ((1.5/ 8) * size.y))
       ..lineTo(((1 / 7) * size.x), ((1.15/ 8) * size.y))
       
       ..close();
