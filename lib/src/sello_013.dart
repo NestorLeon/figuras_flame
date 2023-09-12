0255a3b7702ed7a8544467a50e5b81b238856bfa
chalpm <cponcemejia60@gmail.com>
2023-09-12 16:16:41 -0600

    Se modifico sello_013.dart y figures.dart; Se agrego sello_014.dart

--- lib/src/sello_013.dart
+++ lib/src/sello_013.dart
@@ -17,6 +17,15 @@ class Sello_013 extends PositionComponent {
     super.children,
   }) : super();
 
+  void drawTriangle(Canvas canvas, double x, double y, double size) {
+    Path path = Path();
+    path.moveTo(x, y);
+    path.lineTo(x + size, y);
+    path.lineTo(x + size / 2, y + size);
+    path.close();
+    canvas.drawPath(path, paint);
+  }
+
   @override
   void render(Canvas canvas) {
     super.render(canvas);
@@ -26,48 +35,72 @@ class Sello_013 extends PositionComponent {
     //Dibuja un circulo hueco
     canvas.drawCircle(
       Offset(radio, radio),
-      radio * 0.7,
+      radio,
       paint
-        ..strokeWidth = radio / 60
+        ..strokeWidth = radio / 25
         ..style = PaintingStyle.stroke,
     );
 
     //Dibuja un circulo dentro del original un 80% del tamaño
     canvas.drawCircle(
       Offset(radio, radio),
-      radio * 0.4,
-      paint
-        ..strokeWidth = radio / 14
-        ..style = PaintingStyle.stroke,
-    );
-
-    canvas.drawCircle(
-      Offset(radio, radio),
-      radio * 0.25,
-      paint
-        ..strokeWidth = radio / 12
-        ..style = PaintingStyle.stroke,
-    );
-
-    canvas.drawCircle(
-      Offset(radio, radio),
-      radio * 0.1,
+      radio * 0.6,
       paint
         ..strokeWidth = radio / 10
         ..style = PaintingStyle.stroke,
     );
 
+    canvas.drawCircle(
+      Offset(radio, radio),
+      radio * 0.4,
+      paint
+        ..strokeWidth = radio / 8
+        ..style = PaintingStyle.stroke,
+    );
+
+    canvas.drawCircle(
+      Offset(radio, radio),
+      radio * 0.2,
+      paint
+        ..strokeWidth = radio / 6
+        ..style = PaintingStyle.stroke,
+    );
+    //obtiene las ubicaciones alredor de la circunferencia por dentro
     for (int i = 0; i < 6; i++) {
-      canvas.drawCircle(
-        Offset(
-          radio + radio * 0.57 * cos(i * pi / 3),
-          radio + radio * 0.57 * sin(i * pi / 3),
-        ),
-        radio * 0.05,
+      double x = radio + radio * 0.85 * cos(pi / 3 * i);
+      double y = radio + radio * 0.85 * sin(pi / 3 * i);
+      //Dibuja un 6 mitad de circulo al rededor del original
+      canvas.drawArc(
+        Rect.fromCircle(center: Offset(x, y), radius: radio / 8),
+        i + 1.6,
+        pi,
+        false,
         paint
-          ..strokeWidth = radio / 10
+          ..strokeWidth = radio / 13
           ..style = PaintingStyle.stroke,
       );
     }
+
+    //Calcula la posicion cada 45 grados para dibujar un triangulo
+    for (int i = 0; i < 3; i++) {
+      double x = radio + radio * 0.85 * cos(pi / 3 * i + pi / 6);
+      double y = radio + radio * 0.85 * sin(pi / 3 * i + pi / 6);
+
+      drawTriangle(canvas, x, y, radio / 10);
+    }
+//     flutter: 285.0
+// flutter: 370.0
+// flutter: 285.00000000000006
+// flutter: 115.00000000000004
+// flutter: 30.0
+// flutter: 114.99999999999993
+//         200.0
+// flutter: 52.775681356645464
+// flutter: 52.77568135664541
+// flutter: 199.99999999999997
+// flutter: 347.22431864335454
+//     //drawTriangle(canvas, x, y, size)
+
+    //Gira el triangulo 180 grados
   }
 }
