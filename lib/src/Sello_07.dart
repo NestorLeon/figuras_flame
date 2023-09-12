25e7a2fd7f8dc24c9888ae8a1d11de4cc8d9abf0
BrayanD07 <brayaneve@hotmail.com>
2023-09-12 13:19:49 -0600

    c

--- lib/src/Sello_07.dart
+++ lib/src/Sello_07.dart
@@ -44,7 +44,7 @@ class Sello_07 extends PositionComponent {
 
     final mano = Path()
       ..moveTo(((1 / 9) * size.x), ((1.35/ 8) * size.y))
-      ..lineTo(((1 / 8) * size.x), ((1.35/ 8) * size.y))
+      ..lineTo(((1 / 6) * size.x), ((1.35/ 8) * size.y))
       ..close();
 
       final paint2 = Paint()
