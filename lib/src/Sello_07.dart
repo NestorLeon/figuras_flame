68253938f9640f4ae2fdbf4b65bdd5106b036a90
BrayanD07 <brayaneve@hotmail.com>
2023-09-19 14:39:59 -0600

    c

--- lib/src/Sello_07.dart
+++ lib/src/Sello_07.dart
@@ -79,7 +79,7 @@ class Sello_07 extends PositionComponent {
   final cuerpo= Path()
     
       ..moveTo(((1 / 7) * size.x), ((1.15/ 8) * size.y))
-      ..quadraticBezierTo(((6 / 7) * size.x), ((1.8/ 8) * size.y),((1.7 / 7) * size.x), ((1.9/ 8) * size.y))
+      ..quadraticBezierTo(((5.5 / 7) * size.x), ((1.8/ 8) * size.y),((1.7 / 7) * size.x), ((1.9/ 8) * size.y))
       ..close;
 
     final paint3 = Paint()
