8f48714b75c2ccec70e566b3db212d434505a1c9
BrayanD07 <brayaneve@hotmail.com>
2023-09-19 14:59:57 -0600

    c

--- lib/src/Sello_07.dart
+++ lib/src/Sello_07.dart
@@ -45,10 +45,10 @@ class Sello_07 extends PositionComponent {
 
     final mano = Path()
     //Dedos
-      ..moveTo(((1 / 7) * size.x), ((.85/ 8) * size.y))
+      ..moveTo(((1 / 8) * size.x), ((.85/ 8) * size.y))
       ..lineTo(((1 / 6) * size.x), ((.85/ 8) * size.y))
 
-      ..moveTo(((1 / 8) * size.x), ((1/ 8) * size.y))
+      ..moveTo(((1 / 8.5) * size.x), ((1/ 8) * size.y))
       ..lineTo(((1 / 7) * size.x), ((1/ 8) * size.y))
 
       ..moveTo(((1 / 9) * size.x), ((1.15/ 8) * size.y))
