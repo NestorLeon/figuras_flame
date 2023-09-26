c10c5f58ca7f78384ab2b0c81e451e146e0eac8d
SUSANA TOLENTINO <101066422+Susana-Paola-Tolentino-Hernandez@users.noreply.github.com>
2023-09-26 14:29:48 -0600

    Update figures.dart

--- lib/figures.dart
+++ lib/figures.dart
@@ -22,3 +22,4 @@ export 'src/sello_053.dart' show Sello_053;
 export 'src/sello_013.dart' show Sello_013;
 export 'src/sello_040.dart' show Sello_040;
 export 'src/sello_048.dart' show Sello_048;
+export 'src/sello_056.dart' show Sello_056;
